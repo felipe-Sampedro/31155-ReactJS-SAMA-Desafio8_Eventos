76aefba153b4ca244160cb4d99fffe8b6cff4ab6
felipe.sampedro <pipesancho182@gmail.com>
2022-05-12 22:04:02 -0500

    entrega aplicando eventos proyecto

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,43 @@
+import {useEffect, useState} from 'react'
+/* import Item from '../Item/Item' */
+import ItemList from '../ItemList/ItemList'
+import { catalogo } from '../../Config'
+import { useParams } from 'react-router-dom'
+/* import ItemDetail from '../ItemDetail/ItemDetail' */
+
+const ItemListContainer = ({prods}) => {
+
+
+  const [productos,setProductos] = useState([])
+
+useEffect(() => {
+  const pedido = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve(prods ? prods : catalogo)
+    }, 2000);
+  });
+
+  pedido.then((res) => setProductos(res))
+  .then(()=>console.log(productos))
+  .catch((err)=>console.log(err))
+
+  console.log(pedido);
+
+  return () => {
+    
+  }
+}, [prods])
+
+return (
+    <div className='bg-info'>ItemListContainer
+
+        <p></p>
+        <div className="d-flex justify-content-evenly p-4">
+          <ItemList productos={productos}/>
+        </div>
+
+    </div>
+  )
+}
+
+export default ItemListContainer
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+
+    <div class="spinner-border text-primary m-5" role="status">
+        <span class="sr-only"></span>
+    </div>
+  )
+}
+
+export default Loader
